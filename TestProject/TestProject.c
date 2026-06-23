c6542ef538f5dc2aef726b0f3416a9a4654e1eda
Rafael Caruana <rafaelcaruana@outlook.com>
2026-06-23 21:02:59 +0100

    Update GuessTheNumber.c

--- TestProject/TestProject.c
+++ TestProject/TestProject.c
@@ -7,19 +7,19 @@ int main() {
 
     printf("Guess a number from 1-10: ");
 
-    // Seed the random number generator
+    
     srand(time(NULL));
 
     int min = 1, max = 10;
     int random = (rand() % (max - min + 1)) + min;
 
-    // Read the user's guess
+    
     if (scanf("%d", &choice) != 1) {
         printf("Invalid input.\n");
         return 1;
     }
 
-    // Check the guess
+    
     if (choice == random) {
         printf("Correct! The number was %d.\n", random);
     } else {
