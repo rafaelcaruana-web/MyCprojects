b074597ec2dbb747fde1021a0030e13bedbf8aec
Rafael Caruana <rafaelcaruana@outlook.com>
2026-06-23 19:02:37 +0100

    Refactor main function to include random number guessing

--- TestProject/TestProject.c
+++ TestProject/TestProject.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(void)
-{
-    const char *n[] = {"hi","how","are","you?"};
+int main() {
+    int choice;
 
-    int count = sizeof(n) / sizeof(n[0]);
+    printf("Guess a number from 1-10: ");
 
-    for (int i = 0; i < count; i++) {
-        printf("%s\n", n[i]);
+    // Seed the random number generator
+    srand(time(NULL));
+
+    int min = 1, max = 10;
+    int random = (rand() % (max - min + 1)) + min;
+
+    // Read the user's guess
+    if (scanf("%d", &choice) != 1) {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    // Check the guess
+    if (choice == random) {
+        printf("Correct! The number was %d.\n", random);
+    } else {
+        printf("Wrong! The number was %d.\n", random);
     }
 
     return 0;
 }
-
-#by rafaelcaruana
